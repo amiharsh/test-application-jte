9a69f9410112d19e3900c22790f4e5447fc876b7
Harsh Vardhan <hvkataria12@gmail.com>
2024-06-06 16:55:54 +0530

    first commit

---
+++ libraries/maven/steps/build.groovy
@@ -0,0 +1,5 @@
+void call() {
+    println "Building From Application Maven"
+    println "The message is : ${config.message}"
+    println "THe author is : ${config.author}"
+}
---
+++ libraries/pipeline_utils/steps/checkout_scm.groovy
@@ -0,0 +1,5 @@
+void call() {
+    println "Checkout SCM From Application Called"
+    // List files in the workspace
+    sh "ls -l"
+}

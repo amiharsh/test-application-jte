fc9cdb09865863cc6d0d4e6e409fafc8ec3c501b
Harsh Vardhan <hvkataria12@gmail.com>
2024-06-12 18:12:38 +0530

    Bypassing Fortify

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -18,5 +18,4 @@ application_environments{
 keywords{
     application_name = "WindowsApplication" // Required - Actual Application name (ex. VOR, AMI, BDP, SCP)
     service = "Web" // Required
-    fortify_scan_enabled=false
 }
